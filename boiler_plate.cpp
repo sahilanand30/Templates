3198e62fb99f0f3749adc1799147d89d6fe82dd2
Sahil Anand <80241091+sahilanand30@users.noreply.github.com>
2022-01-11 20:27:23 +0530

    Add files via upload

---
+++ boiler_plate.cpp
@@ -0,0 +1,30 @@
+//JAY MAA CHAMUNDA
+//AUTHOR : Sahil Anand
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define ull unsigned long long int
+#define LL_MAX 9223372036854775807
+#define LL_MIN -9223372036854775808
+#define PI 3.1415926536
+#define mod 1000000007
+#define ln "\n"
+#define FASTIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define not_standard                  \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+typedef tree<pair<ll, ll>, null_type, less<pair<ll, ll>>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+/*-------------------------------------------MAIN CODE-------------------------------------------*/
+
+int main()
+{
+    FASTIO
+    
+    return 0;
+}
