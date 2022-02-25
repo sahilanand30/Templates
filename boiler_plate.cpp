d4c3c91356d47a8ee29398706a3e12dc85022f63
Sahil Anand <80241091+sahilanand30@users.noreply.github.com>
2022-02-25 17:57:49 +0530

    Update boiler_plate.cpp

--- boiler_plate.cpp
+++ boiler_plate.cpp
@@ -1,37 +1,64 @@
-//JAY MAA CHAMUNDA
-//AUTHOR : Sahil Anand
-/************************************************************
-Important stuff to keep in mind:-
-(1)if n=0 , if n=1, if n=maximum, if n is even, if n is odd ?
-(2)Can negative values for 'n' is possible?
-************************************************************/
+// JAY MAA CHAMUNDA
+// AUTHOR : Sahil Anand
+/*-----------------------------------------------{HEADERS}-----------------------------------------------*/
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+/*------------------------------------------------{MACROS}------------------------------------------------*/
 using namespace std;
 using namespace __gnu_pbds;
+#define rep(i, n) for (ll i = 0; i < n; i++)
 #define all(x) x.begin(), x.end()
 #define ll long long int
 #define ull unsigned long long int
 #define countSetBits(z) __builtin_popcountll(z);
 #define vll vector<long long int>
-#define LL_MAX 9223372036854775807
+#define LL_MAX __LONG_LONG_MAX__
 #define LL_MIN -9223372036854775808
 #define PI 3.1415926536
 #define mod 1000000007
+#define INF 1e18
 #define nl "\n"
+/*-----------------------------------------------{FAST I/O}-----------------------------------------------*/
 #define FASTIO                        \
     ios_base::sync_with_stdio(false); \
     cin.tie(0);
+/*-------------------------------------------{NON-STANDARD I/O}-------------------------------------------*/
 #define not_standard                  \
     freopen("input.txt", "r", stdin); \
     freopen("output.txt", "w", stdout);
+/*-------------------------------------------{PBDS/ORDERED_SET}-------------------------------------------*/
 typedef tree<pair<ll, ll>, null_type, less<pair<ll, ll>>, rb_tree_tag, tree_order_statistics_node_update> pbds;
-/*-------------------------------------------{MAIN CODE}-------------------------------------------*/
+/*------------------------------------------{OPERATOR_OVERLOADS}------------------------------------------*/
+template <typename T1, typename T2> // cin >> pair<T1, T2>
+istream &operator>>(istream &istream, pair<T1, T2> &p)
+{
+    return (istream >> p.first >> p.second);
+}
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T1, typename T2> // cout << pair<T1, T2>
+ostream &operator<<(ostream &ostream, const pair<T1, T2> &p)
+{
+    return (ostream << p.first << " " << p.second);
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c)
+{
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+/*-----------------------------------------------{MAIN CODE}-----------------------------------------------*/
 
 int main()
 {
     FASTIO
-    
+
     return 0;
 }
