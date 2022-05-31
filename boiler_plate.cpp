8b629762152e7b0f10bba1cf3d735255ac79d58a
Sahil Anand <80241091+sahilanand30@users.noreply.github.com>
2022-05-31 11:57:21 +0530

    Update boiler_plate.cpp

--- boiler_plate.cpp
+++ boiler_plate.cpp
@@ -4,6 +4,7 @@ APPROACHES (ideas):-
 ->Simulation / Brute Force ?? (check constraints)
 ->Binary search the answer / Binary search on answer ??
 ->Prefix Sum / Suffix Sum ??
+->Sliding Window ??
 ->Dp / Greedy ??
 ->Bit Manipulation ??
 ->Parity (odd/even) ??
